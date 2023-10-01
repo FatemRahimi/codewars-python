8535cd81c7980ed5ac94f21d80f935ce918a6d20
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-01 11:37:09 +0100

    number star ladder

--- numberStar-ladder.py
+++ numberStar-ladder.py
@@ -9,3 +9,12 @@
 # 1
 # 1*2
 # 1**3
+def pattern(n):
+    s = ''
+    for i in range(n):
+        if i == 0:
+            s += '1\n'
+        else:
+            s += '1{}{}\n'.format('*' * i, i + 1)
+    
+    return s.rstrip('\n')
