6e02a550f872eb5d39c1ca1756b9f39057a2a46a
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-07 23:51:54 +0100

    codes

--- consecutiveLetter.py
+++ consecutiveLetter.py
@@ -11,3 +11,17 @@
 # All inputs will be lowercase letters.
 
 # More examples in test cases. Good luck!
+
+
+def solve(st):
+    b = list(st)
+    c = []
+    for i in range(len(st)):
+        c.append(ord(b[i]))
+    for i in c:
+        if c.count(i) != 1:
+            return False
+    for i in range(min(c),max(c)):
+        if i+1 not in c:
+            return False
+    return True
