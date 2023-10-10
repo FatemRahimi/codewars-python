b2d1512e0d5d2d50fa52f2abd70b29242f7fda0f
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-10 23:01:11 +0100

    tested

--- mystreiesfunction.py
+++ mystreiesfunction.py
@@ -14,15 +14,3 @@ def get_num(n):
     return sum
 
 # tested
-import codewars_test as test
-from solution import get_num
-
-@test.describe("Fixed Tests")
-def fixed_tests():
-    @test.it('Basic Test Cases')
-    def basic_test_cases():
-        test.assert_equals(get_num(300), 2, "Wrong output!")
-        test.assert_equals(get_num(90783), 4, "Wrong output!")
-        test.assert_equals(get_num(123321), 0, "Wrong output!")
-        test.assert_equals(get_num(89282350306), 8, "Wrong output!")
-        test.assert_equals(get_num(3479283469), 5, "Wrong output!")
