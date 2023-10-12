f553bbd3b391e9f3735dab19c85b77ac28168eb7
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-12 22:59:39 +0100

    tested

--- evenorodd.js
+++ evenorodd.js
@@ -1,3 +1,25 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 def even_or_odd(number):
   return 'Even' if number % 2 == 0 else 'Odd'
+
+
+//   test
+
+import codewars_test as test
+from solution import even_or_odd
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(even_or_odd(2), "Even")
+        test.assert_equals(even_or_odd(1), "Odd")
+        test.assert_equals(even_or_odd(0), "Even")
+        test.assert_equals(even_or_odd(1545452), "Even")
+        test.assert_equals(even_or_odd(7), "Odd")
+        test.assert_equals(even_or_odd(78), "Even")
+        test.assert_equals(even_or_odd(17), "Odd")
+        test.assert_equals(even_or_odd(74156741), "Odd")
+        test.assert_equals(even_or_odd(100000), "Even")
+        test.assert_equals(even_or_odd(-123), "Odd")
+        test.assert_equals(even_or_odd(-456), "Even")
