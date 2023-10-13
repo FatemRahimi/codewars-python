af9476538367968957d0e22866486024af3c21f7
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-13 23:00:58 +0100

    tested again

--- collatzCongecture.py
+++ collatzCongecture.py
@@ -29,3 +29,14 @@ def hotpo(n):
     return cnt
 
 # tested
+import codewars_test as test
+from solution import hotpo
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(hotpo(1), 0);
+        test.assert_equals(hotpo(5), 5);
+        test.assert_equals(hotpo(6), 8);
+        test.assert_equals(hotpo(23), 15);
