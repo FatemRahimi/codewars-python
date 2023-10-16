97c2e9eb5bae8d6b0b14926c017c276eb0cc118a
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-16 23:28:24 +0100

    solution

--- dayofyear.py
+++ dayofyear.py
@@ -8,3 +8,23 @@
 # The year can be evenly divided by 4;
 # If the year can be evenly divided by 100, it is NOT a leap year, unless;
 # The year is also evenly divisible by 400. Then it is a leap year.
+def to_day_of_year(date):
+    
+    days,months,year = date
+    
+    for month in range(months-1): 
+        if month == 1:
+            # checks if it is a leap year or not
+            if (year % 4 == 0) and (year % 100 != 0):
+                days += 29
+            elif (year % 100 == 0) and (year % 400 == 0): 
+                days += 29
+            else: 
+                days += 28
+        else:
+            if month == 3 or month == 5 or month == 8 or month == 10:  # months with 30days; april, june, september and november respectively
+                days += 30
+            else:
+                days += 31
+     
+    return days
