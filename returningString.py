2bb84d7d90ab49d7494d639fc42e3ff8577ae28d
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-18 23:45:03 +0100

    tested

--- returningString.py
+++ returningString.py
@@ -13,3 +13,16 @@ def greet(name):
 
 # def greet(name):
 #     return 'Hello, ' + name + ' how are you doing today?'
+
+
+
+# tested
+import codewars_test as test
+from solution import greet
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(greet('Ryan'), "Hello, Ryan how are you doing today?")
+        test.assert_equals(greet('Shingles'), "Hello, Shingles how are you doing today?")
