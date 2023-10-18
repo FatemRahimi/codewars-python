fa55fd5646242b8fa849f4b335d2995964e6641d
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-18 23:44:19 +0100

    solution

--- returningString.py
+++ returningString.py
@@ -2,3 +2,14 @@
 # Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
 # [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+def greet(name):
+    return "Hello, {} how are you doing today?".format(name)
+
+# for python 3.6
+# def greet(name):
+#     return f'Hello, {name} how are you doing today?'
+
+
+# def greet(name):
+#     return 'Hello, ' + name + ' how are you doing today?'
