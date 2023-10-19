cb4ba78538d319cb1b284adc9889cf5422772f1f
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-19 22:33:30 +0100

    tested

--- simpleRemoveDoplicate.py
+++ simpleRemoveDoplicate.py
@@ -19,3 +19,17 @@ def solve(arr):
         if i not in re:
             re.append(i)
     return re[::-1]
+
+import codewars_test as test
+from solution import solve
+
+
+@test.describe("Basic Tests")
+def basic_tests():
+    
+    @test.it("Basic Tests")
+    def basic_tests():
+        test.assert_equals(solve([3,4,4,3,6,3]),[4,6,3])
+        test.assert_equals(solve([1,2,1,2,1,2,3]),[1,2,3])
+        test.assert_equals(solve([1,2,3,4]),[1,2,3,4])
+        test.assert_equals(solve([1,1,4,5,1,2,1]),[4,5,2,1])
