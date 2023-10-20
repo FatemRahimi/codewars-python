5f07d81b324dfb2bef0ee0b8aec06f9b883e59b9
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-20 23:02:36 +0100

    tested

--- IsGonna.py
+++ IsGonna.py
@@ -14,4 +14,16 @@ def hero(bullets, dragons):
         return True
     elif bullets < 2*dragons:
         return False
-    
+    import codewars_test as test
+from solution import hero
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(hero(10, 5), True)
+        test.assert_equals(hero(7, 4), False)
+        test.assert_equals(hero(4, 5), False)
+        test.assert_equals(hero(100, 40), True)
+        test.assert_equals(hero(1500, 751), False)
+        test.assert_equals(hero(0, 1), False)
