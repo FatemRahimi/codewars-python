871558e8f824254d124a0838d36c12173bc822e0
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-21 22:34:12 +0100

    tested

--- powersOff.py
+++ powersOff.py
@@ -11,3 +11,16 @@ from solution import pofi
 @test.describe("Basic Tests")
 def basic_tests():
     
+    @test.it("Basic Tests")
+    def basic_tests():
+        test.assert_equals(pofi(0),'1')
+        test.assert_equals(pofi(1),'i')
+        test.assert_equals(pofi(2),'-1')
+        test.assert_equals(pofi(3),'-i')
+        test.assert_equals(pofi(4),'1')
+        test.assert_equals(pofi(5),'i')
+        test.assert_equals(pofi(6),'-1')
+        test.assert_equals(pofi(7),'-i')
+        test.assert_equals(pofi(8),'1')
+        test.assert_equals(pofi(9),'i')
+        test.assert_equals(pofi(10),'-1')
