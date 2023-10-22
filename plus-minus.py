e63d8c7e71ef39cc741e33f9a0640bf3a05b42eb
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-22 21:08:41 +0100

    solution

--- plus-minus.py
+++ plus-minus.py
@@ -17,3 +17,10 @@
 # */
 
 # catchSignChange(arr) == 2;
+
+def catch_sign_change(lst):
+    count = 0
+    for i in range(1,len(lst)):
+        if lst[i] < 0 and lst[i-1] >= 0:count += 1
+        if lst[i] >= 0 and lst[i-1] < 0:count += 1
+    return count
