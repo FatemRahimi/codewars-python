b19d08bfc4f96ef3ebbc511dbd4c513f325e2b21
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-01 21:49:36 +0000

    fix tested

--- findTheDoplicate.py
+++ findTheDoplicate.py
@@ -16,12 +16,3 @@ function findDup( arr ){
   }
 }
 
-
-const Test = require('@codewars/test-compat');
-
-describe("Tests", () => {
-  it("test", () => {
-Test.assertEquals(findDup([1,2,2,3]), 2);
-Test.assertEquals(findDup([1,3,2,5,4,5,7,6]), 5);
-  });
-});
