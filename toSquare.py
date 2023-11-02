8277760443e96cadb8caa1f055e2e3c210c459e8
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-02 22:45:26 +0000

    fix tested

--- toSquare.py
+++ toSquare.py
@@ -17,3 +17,20 @@ function squareOrSquareRoot(array) {
 }
 
 
+const Test = require('@codewars/test-compat');
+
+describe("Solution", function(){
+  it("basic tests", function(){
+    var input = [ 4, 3, 9, 7, 2, 1 ];
+    var expected = [ 2, 9, 3, 49, 4, 1 ];
+    Test.assertSimilar(squareOrSquareRoot(input), expected);
+    
+    input = [ 100, 101, 5, 5, 1, 1 ];
+    expected = [ 10, 10201, 25, 25, 1, 1 ];
+    Test.assertSimilar(squareOrSquareRoot(input), expected);
+    
+    input = [ 1, 2, 3, 4, 5, 6 ];
+    expected = [ 1, 4, 9, 2, 25, 36 ];
+    Test.assertSimilar(squareOrSquareRoot(input), expected);
+  });
+});
