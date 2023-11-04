c817290fa64e3cc7d19ff518812606516c4aa235
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-04 00:04:46 +0000

    solution

--- marginStore.py
+++ marginStore.py
@@ -1,2 +1,9 @@
 # Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
 
+function mergeArrays(a, b) {
+    let merge = [...a, ...b].sort()
+    let unique = new Set(merge)
+    return Array.from(unique).sort((a,b)=> a-b)
+}
+Best Practices7Clever1
+ 0ForkCompare with your solutionLink
