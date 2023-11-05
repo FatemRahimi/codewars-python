723cb45894d6966cff6457ad29e6463b55680db9
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-05 23:25:00 +0000

    create a file

--- marginStore.py
+++ marginStore.py
@@ -9,3 +9,13 @@ Best Practices7Clever1
  0ForkCompare with your solutionLink
 
 # tested
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("mergeArrays", function(){
+  it("Example Tests", function(){
+    assert.deepEqual(mergeArrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]);
+    assert.deepEqual(mergeArrays([2, 4, 8], [2, 4, 6]), [2, 4, 6, 8]);
+  });
+});
