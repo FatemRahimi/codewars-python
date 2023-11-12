7595ee007879e735aefd49f9b3a801054855f70c
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-12 21:04:39 +0000

    tested

--- catyears.py
+++ catyears.py
@@ -37,3 +37,21 @@ const humanYearsCatYearsDogYears = (humanYears) => {
   }
   return [humanYears,catYears,dogYears];
 }
+const Test = require('@codewars/test-compat');
+
+describe("Example Tests", function() {
+
+  it("one", function() {
+    Test.assertSimilar(humanYearsCatYearsDogYears(1), [1,15,15]);
+  });
+
+  it("two", function() {
+    Test.assertSimilar(humanYearsCatYearsDogYears(2), [2,24,24]);
+  });
+
+  it("ten", function() {
+    Test.assertSimilar(humanYearsCatYearsDogYears(10), [10,56,64]);
+  });
+
+});
+
