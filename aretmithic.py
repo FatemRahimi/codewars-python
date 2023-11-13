e1f1c450d9ca4aae1d7fb9dc914c3f3c5c5613a0
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-13 23:01:36 +0000

    tested

--- aretmithic.py
+++ aretmithic.py
@@ -16,3 +16,11 @@ var seqlist = function(first,c,l){
   }
   return result;
 }
+#tested
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertSimilar(seqlist(0,1,20),[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])
+  });
+});
