ea05bcc14752903185b54a66b729bf391089d518
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-17 00:14:48 +0000

    solution

--- sexi.py
+++ sexi.py
@@ -7,3 +7,16 @@
 # 5, 7    -->  false
 # 1, 7    -->  false  (1 is not a prime)
 # Note: x & y are always positive integers, but they are not always in order of precendence... For example you can be given either (5, 11) or (11, 5) - both are valid.
+
+
+function sexy_prime(x, y){
+  for (var i=2; i<x; ++i)
+    if (x%i==0)
+      return false;
+  for (var i=2; i<y; ++i)
+    if (y%i==0)
+      return false;    
+  if (y==1 ||x==1)
+    return false;
+  return Math.abs(x-y)==6
+}
