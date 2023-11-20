7e582aacea70a92c40ab4340a4a55415be3d6746
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-20 20:33:34 +0000

    tested

--- isItaNumber.py
+++ isItaNumber.py
@@ -14,3 +14,16 @@
 function isDigit(s) {
  return s==parseFloat(s);
 }
+
+# test
+const Test = require('@codewars/test-compat');
+
+describe("Solution", function(){
+  it("basic tests", function(){
+    var input = "s2324";
+    Test.assertEquals(isDigit(input), false);
+    
+    input = "-234.4";    
+    Test.assertEquals(isDigit(input), true);    
+  });
+});
