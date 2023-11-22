17762c88b3e965b78251a7969ba0698b136ef0b6
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-22 19:45:39 +0000

    solution

--- cravityFlip.py
+++ cravityFlip.py
@@ -18,3 +18,8 @@
 # Examples (input -> output:
 # * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
 # * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+
+const flip=(d, a)=>{
+  if(d === 'R') return a.sort((a,b)=>a-b);
+  if(d === 'L') return a.sort((a,b)=>b-a);
+}
