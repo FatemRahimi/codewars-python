8b9bda79bcad731bb0a63b74eb21ae366677cc5e
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-23 21:25:26 +0000

    structure

---
+++ sumLenght.py
@@ -0,0 +1,7 @@
+# In this kata you must return a string value, where the first part is the sum of positive numbers and the second part is the number of negative numbers.
+# Knowing that the first '0' is negative, the second is positive, the third is negative, and so on...
+
+
+# For example :
+# sumLength([-1,2,3,4,0,1,0,-2,0,-3])
+# return '10 5' (10 = 1 + 2 + 3 + 4 and 5 = 3 + 2 => 3 negatives numbers + second 0 as negative)
