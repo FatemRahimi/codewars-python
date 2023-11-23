931a1e1dbeb04f40f12106cbc89736c83981391f
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-23 21:26:09 +0000

    solution

--- sumLenght.py
+++ sumLenght.py
@@ -5,3 +5,10 @@
 # For example :
 # sumLength([-1,2,3,4,0,1,0,-2,0,-3])
 # return '10 5' (10 = 1 + 2 + 3 + 4 and 5 = 3 + 2 => 3 negatives numbers + second 0 as negative)
+
+function sumLength (arr) {
+  const sum = arr.filter((x, i) => x > 0).reduce((a, b) => a + b, 0);
+  let z = 0;
+  const len = arr.filter((x, i) => x < 0 || (x == 0 && (z++) % 2 == 0)).length;
+  return `${sum} ${len}`
+}
