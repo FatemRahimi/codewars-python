5750559033de598d66c8cf65c792695b8aba5be8
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-26 22:07:38 +0000

    tested

--- twicOld.py
+++ twicOld.py
@@ -7,3 +7,15 @@ def twice_as_old(dad_years_old, son_years_old):
     return abs(dad_years_old - 2*son_years_old)
 
 test
+import codewars_test as test
+from solution import twice_as_old
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(twice_as_old(36,7) , 22)
+        test.assert_equals(twice_as_old(55,30) , 5)
+        test.assert_equals(twice_as_old(42,21) , 0)
+        test.assert_equals(twice_as_old(22,1) , 20)
+        test.assert_equals(twice_as_old(29,0) , 29)
