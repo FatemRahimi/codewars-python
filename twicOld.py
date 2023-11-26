e7a39609a02f582f0127bf23390bb90abf7153e7
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-26 22:07:25 +0000

    test

--- twicOld.py
+++ twicOld.py
@@ -5,3 +5,5 @@
 # Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
 def twice_as_old(dad_years_old, son_years_old):
     return abs(dad_years_old - 2*son_years_old)
+
+test
