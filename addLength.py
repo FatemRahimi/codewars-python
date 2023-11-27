9a13f15b666a23c936d04342ec865dd31f7705ab
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-27 20:55:10 +0000

    add test

--- addLength.py
+++ addLength.py
@@ -8,7 +8,7 @@
 # Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 
 # Note: String will have at least one element; words will always be separated by a space.
-
+# tetst
 def add_length(str_):
     answer = []
     for word in str_.split():
