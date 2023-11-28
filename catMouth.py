eaa0a10bea744469d08956476a4e5e94c1666c60
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-28 21:44:13 +0000

    structure

--- catMouth.py
+++ catMouth.py
@@ -23,3 +23,18 @@
 # returns "Escaped!" because the cat cannot catch the mouse in  5 moves
 
 # solution
+
+
+def cat_mouse(map_, moves):
+    if 'C' not in map_ or 'm' not in map_:
+        return 'boring without two animals'
+    
+    for row, line in enumerate(map_.splitlines()):
+        if 'C' in line:
+            cat = row, line.index('C')
+        if 'm' in line:
+            mouse = row, line.index('m')
+    
+    distance = abs(cat[0] - mouse[0]) + abs(cat[1] - mouse[1])
+    
+    return 'Caught!' if distance <= moves else 'Escaped!'
