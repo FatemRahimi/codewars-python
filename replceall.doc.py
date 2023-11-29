f9da5cbe0d6e96e89858e63cd36eb1a4ec1c35d5
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-29 23:30:40 +0000

    tested

--- replceall.doc.py
+++ replceall.doc.py
@@ -11,3 +11,16 @@
 import re
 def replace_dots(str):
     return re.sub(r"\.", "-", str)
+
+
+import codewars_test as test
+from solution import replace_dots
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(replace_dots(""), "")
+        test.assert_equals(replace_dots("no dots"), "no dots")
+        test.assert_equals(replace_dots("one.two.three"), "one-two-three")
+        test.assert_equals(replace_dots("........"), "--------")
