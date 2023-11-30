35e68d74ff98bc6c59478cfb1b37dc2c28abb11f
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-11-30 21:48:07 +0000

    solution

--- theNewestFunction.py
+++ theNewestFunction.py
@@ -3,3 +3,6 @@
 # Your assignment today is to write a function in JavaScript. It seems simple enough, but the CEO insists that all developers who work at his company only use the absolute cutting-edge of technology. You must use only the newest of features and frameworks. But such new software tends to be rife with bugs...
 
 # Create a function that won't throw an error no matter how new it is.
+const newFunction = function noWar() {
+  return noWar;
+}
