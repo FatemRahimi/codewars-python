3240566062cb5962b424d07ba9b63d23476cb330
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-12-02 22:45:01 +0000

    solution

--- whoTogo.py
+++ whoTogo.py
@@ -2,3 +2,6 @@
 
 # Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is"
 
+function whoIsPaying(name){
+  return (name.length>2)?([name, name.substr(0,2)]):[name];
+}
