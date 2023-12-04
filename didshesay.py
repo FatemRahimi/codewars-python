89548e476e74f215f4f2a6b62f46f710bba72787
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-12-04 22:26:36 +0000

    solution

--- didshesay.py
+++ didshesay.py
@@ -36,3 +36,24 @@ const validateHello = greetings => {
   }
   return false
 }
+
+test
+
+const hellos = {
+hello: 'english',
+ciao: 'italian',
+salut: 'french',
+hallo: 'german',
+hola: 'spanish',
+ahoj: 'czech republic',
+czesc: 'polish',
+}
+
+const validateHello = greetings => {
+  for (key in hellos) {
+   if (greetings.toLowerCase().includes(key)) {
+     return true
+   }
+  }
+  return false
+}
