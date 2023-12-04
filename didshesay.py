0536a25c906e489d630cb5c48b2ca07e1dba9c2b
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-12-04 22:25:45 +0000

    solution

--- didshesay.py
+++ didshesay.py
@@ -16,3 +16,23 @@
 # you can assume the input is a string.
 # to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
 # function should be case insensitive to pass the tests
+
+const hellos = {
+hello: 'english',
+ciao: 'italian',
+salut: 'french',
+hallo: 'german',
+hola: 'spanish',
+ahoj: 'czech republic',
+czesc: 'polish',
+}
+
+
+const validateHello = greetings => {
+  for (key in hellos) {
+   if (greetings.toLowerCase().includes(key)) {
+     return true
+   }
+  }
+  return false
+}
