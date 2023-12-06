ec906c040a5b061c66c7ba44cea61416a468ecd8
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-12-06 23:41:23 +0000

    structure

---
+++ objectOrient.py
@@ -0,0 +1,9 @@
+# Object-Oriented PHP #2 - Class Constructors and $this
+# About this Kata Series
+# In this series, we will start off by learning the very fundamentals of object-oriented programming (commonly referred to as "OOP") and classes in PHP. We will then proceed by learning about the 3 different visibilities of class properties and methods and when to use them. After that, classical inheritance will be taught along with its perks and potential drawbacks. Finally, in the final Kata(s) in this series, more advanced OOP concepts may be taught such as interfaces and even abstract classes. Apart from the main course content, every 3 Kata or so into this Series, there will be an exercise Kata to practice on previously learned knowledge.
+
+# Lesson
+# Recap
+# In the previous Kata of this series, we learned the fundamental concepts of Object-Oriented Programming (OOP) in PHP and its advantages over functional programming. We then proceeded to learn the syntax for defining classes in PHP and learned that you can define variables and functions (called "properties" and "methods") inside a class in which we can then access by creating objects (or instances of that class) from it.
+
+# However, last lesson, all the properties that we defined in a class had a predefined value. For example, you may remember that in the President class you defined, all presidents (i.e. all instances of your President class) had the name "Barack Obama". Not very useful is it? What if we could change and customize the names of our presidents, like such (see below)?
