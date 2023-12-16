4e7732bf592671bd6442f3aba0bf55a0f4fba401
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-12-16 19:40:28 +0000

    tested

--- findOccurency.py
+++ findOccurency.py
@@ -15,3 +15,16 @@ def find_all(array, n):
         if array[i] == n:
             res.append(i)
     return res
+# test
+
+import codewars_test as test
+from solution import find_all
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(find_all([6, 9, 3, 4, 3, 82, 11], 3), [2, 4])
+        test.assert_equals(find_all([6, 9, 3, 4, 3, 82, 11], 99), [])
+        test.assert_equals(find_all([10, 16, 20, 6, 14, 11, 20, 2, 17, 16, 14], 16), [1, 9])
+        test.assert_equals(find_all([20, 20, 10, 13, 15, 2, 7, 2, 20, 3, 18, 2, 3, 2, 16, 10, 9, 9, 7, 5, 15, 5], 20), [0, 1, 8])
