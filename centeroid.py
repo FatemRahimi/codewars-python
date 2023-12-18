5719ac06e6932ca9e0a8f1eafdcea27869b37679
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-12-18 23:59:37 +0000

    test

--- centeroid.py
+++ centeroid.py
@@ -8,3 +8,12 @@ def centroid(c):
     return np.mean(c, axis=0).round(2).tolist()
 
 # test
+import codewars_test as test
+from solution import centroid
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(centroid([[1,0,5], [0,1,5], [2,2,5]]), [1.0, 1.0, 5.0])
+        test.assert_equals(centroid([[7,0,5], [3,1,5], [2,1,5]]), [4.0, 0.67, 5.0])
