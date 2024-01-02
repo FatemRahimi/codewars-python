781d72dffb7b351faabe777f4975067f1cb709f4
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-01-02 19:41:06 +0000

    tested

--- safenuser.py
+++ safenuser.py
@@ -18,3 +18,18 @@ ef html_special_chars(data):
         '&': '&amp;',
     }       
     return ''.join(specials.get(char, char) for char in data)
+
+
+import codewars_test as test
+from solution import html_special_chars
+import string
+from random import randint, choices
+
+@test.describe("Example")
+def test_group():
+    @test.it("Testing Simple Examples")
+    def simple_test_cases():
+        test.assert_equals(html_special_chars("<h2>Hello World</h2>"), "&lt;h2&gt;Hello World&lt;/h2&gt;")
+        test.assert_equals(html_special_chars("Hello, how would you & I fare?"), "Hello, how would you &amp; I fare?")
+        test.assert_equals(html_special_chars('How was "The Matrix"?  Did you like it?'), 'How was &quot;The Matrix&quot;?  Did you like it?')
+        test.assert_equals(html_special_chars("<script>alert('Website Hacked!');</script>"), "&lt;script&gt;alert('Website Hacked!');&lt;/script&gt;")
