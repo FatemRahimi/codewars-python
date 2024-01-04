c8cd349fef08e66755979d435634025deb067e5f
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-01-04 23:32:49 +0000

    tested

--- exclamation.py
+++ exclamation.py
@@ -23,3 +23,31 @@ def remove(s, n):
             else:
                 liste.append(s[i])
     return "".join(liste)
+
+# test
+import codewars_test as test
+from solution import remove
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+
+        tests = [
+            #[[input], [expected]],
+            [["Hi!",1] , "Hi"],
+            [["Hi!",100] , "Hi"],
+            [["Hi!!!",1] , "Hi!!"],
+            [["Hi!!!",100] , "Hi"],
+            [["!Hi",1] , "Hi"],
+            [["!Hi!",1] , "Hi!"],
+            [["!Hi!",100] , "Hi"],
+            [["!!!Hi !!hi!!! !hi",1] , "!!Hi !!hi!!! !hi"],
+            [["!!!Hi !!hi!!! !hi",3] , "Hi !!hi!!! !hi"],
+            [["!!!Hi !!hi!!! !hi",5] , "Hi hi!!! !hi"],
+            [["!!!Hi !!hi!!! !hi",100] , "Hi hi hi"],
+        ]
+
+
+        for inp, exp in tests:
+            test.assert_equals(remove(*inp), exp)
