7e79e6ef41c576ee518ad72412d78a2b439eb5bb
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-01-07 23:03:14 +0000

    tested

--- password.py
+++ password.py
@@ -42,3 +42,10 @@ def help_zoom(key):
             return "No"
 
 # test
+        
+        test.describe("Basic tests")
+test.assert_equals(help_zoom([1, 1, 0, 0, 0, 0, 0, 1, 1 ]) , "Yes")
+test.assert_equals(help_zoom([1, 1, 0, 0, 0, 0, 1, 1, 0 ]) , "No")
+test.assert_equals(help_zoom([1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1 ]) , "Yes")    
+test.assert_equals(help_zoom([1, 0, 1, 1, 0, 0, 0, 0, 0 ]) , "No")
+test.assert_equals(help_zoom([1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1 ]) , "No")
