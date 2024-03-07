919179a768530c19c36a8292657723c3318ccd04
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-03-07 22:58:27 +0000

    solution

--- BuildingSpheres.py
+++ BuildingSpheres.py
@@ -15,3 +15,21 @@
 # ball.get_surface_area() -> 50.26548
 # ball.get_density() ->      1.49208
 # Any feedback would be much appreciated
+
+from math import pi
+class Sphere(object):
+    def __init__(self, radius, mass):
+        self.radius = radius
+        self.mass = mass
+        self.volume = 4*pi * self.radius**3 / 3
+        self.surface = 4*pi* self.radius**2
+    def get_radius(self):
+        return self.radius
+    def get_mass(self):
+        return self.mass
+    def get_volume(self):
+        return round(self.volume,5)
+    def get_surface_area(self):
+        return round(self.surface,5)
+    def get_density(self):
+        return round(self.mass/self.volume, 5)
