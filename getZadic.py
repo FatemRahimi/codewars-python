0b1f935b06d7d165806c445e7bc4c7ea7a3956c6
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-03-15 22:33:30 +0000

    solution

--- getZadic.py
+++ getZadic.py
@@ -23,3 +23,25 @@
 # Capricorn (December 22-January 19)
 # Aquarius (January 20 to February 18)
 # Pisces (February 19 to March 20)
+
+
+
+ZODIAC = (
+    (11, 119, "Capricorn", "1 January - 19 January"),
+    (120, 218, "Aquarius", "20 January - 18 February"),
+    (219, 320, "Pisces", "19 February - 20 March"),
+    (321, 419, "Aries", "21 March - 19 April"),
+    (420, 520, "Taurus", "20 April - 20 May"),
+    (521, 620, "Gemini", "21 May - 20 June"),
+    (621, 722, "Cancer", "21 June - 22 July"),
+    (723, 822, "Leo", "23 July - 22 August"),
+    (823, 922, "Virgo", "23 August - 22 September"),
+    (923, 1022, "Libra", "23 September - 22 October"),
+    (1023, 1121, "Scorpio", "23 October - 21 November"),
+    (1122, 1221, "Sagittarius", "22 November - 21 December"),
+    (1222, 1231, "Capricorn", "22 December - 31 December")
+)
+
+def get_zodiac_sign(day, month):
+    interval = int(f"{month}" + f"{day:02d}")
+    return next((zodiac[2] for zodiac in ZODIAC if zodiac[0] <= interval <= zodiac[1]), "Unknown")
