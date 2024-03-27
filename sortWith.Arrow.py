6203a2811cd85d646a124190c4647b6e54a0c270
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-03-27 23:23:10 +0000

    test

--- sortWith.Arrow.py
+++ sortWith.Arrow.py
@@ -6,6 +6,28 @@
 # arr = [2,4,6,8,1,2,5,4,3,2]
 
 # dropWhile(arr, isEven) == [1,2,5,4,3,2] # True
+from itertools import dropwhile
 
 def drop_while(arr, pred):
-    pass #your code here
+    return list(dropwhile(pred, arr))
+
+
+from solution import drop_while
+import codewars_test as test
+
+@test.it("Basic tests")
+def basic_tests():
+    
+    is_even=lambda n: not n%2
+    is_odd=lambda n: n%2
+    
+    test.assert_equals(drop_while([2,6,4,10,1,5,4,3], is_even),[1,5,4,3])
+    test.assert_equals(drop_while([2,100,1000,10000,10000,5,3,4,6], is_even),[5,3,4,6])
+    test.assert_equals(drop_while([998,996,12,-1000,200,0,1,1,1], is_even),[1,1,1])
+    test.assert_equals(drop_while([1,4,2,3,5,4,5,6,7], is_even),[1,4,2,3,5,4,5,6,7])
+    test.assert_equals(drop_while([2,4,10,100,64,78,92], is_even),[])
+    test.assert_equals(drop_while([1,2,3,4,5], is_odd),[2,3,4,5])
+    test.assert_equals(drop_while([1,5,111,1111,1111,2,4,6,4,5], is_odd),[2,4,6,4,5])
+    test.assert_equals(drop_while([-1,-5,127,86,902,2,1], is_odd),[86,902,2,1])
+    test.assert_equals(drop_while([2,1,2,4,3,5,4,6,7,8,9,0], is_odd),[2,1,2,4,3,5,4,6,7,8,9,0])
+    test.assert_equals(drop_while([1,3,5,7,9,111], is_odd),[])
