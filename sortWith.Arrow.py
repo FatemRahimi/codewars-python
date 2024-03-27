6a36889208640568684678f0fac25a06e9da4ff8
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-03-27 23:19:27 +0000

    instruction

--- sortWith.Arrow.py
+++ sortWith.Arrow.py
@@ -1,9 +1,8 @@
-# Order People by age Using Arrow Function
-# Sort and Order people by their age using Arrow Functions
-# Task
-# Your task is to order a list containg people objects by age using the new Javascript Arrow Functions
-# Input
-# Input will be a valid array with People objects containing an Age and Name
-# Output
-# Output will be a valid sorted array with People objects sorted by Age in ascending order
-# Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+# Yet another staple for the functional programmer. You have a sequence of values and some predicate for those values. You want to remove the longest prefix of elements such that the predicate is true for each element. We'll call this the dropWhile function. It accepts two arguments. The first is the sequence of values, and the second is the predicate function. The function does not change the value of the original sequence.
+
+# def isEven(num):
+#   return num % 2 == 0
+
+# arr = [2,4,6,8,1,2,5,4,3,2]
+
+# dropWhile(arr, isEven) == [1,2,5,4,3,2] # True
