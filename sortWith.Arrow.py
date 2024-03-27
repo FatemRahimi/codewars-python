6db72d213e91a983cba603b96fb048627da1ac56
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-03-27 23:12:30 +0000

    structure

---
+++ sortWith.Arrow.py
@@ -0,0 +1,9 @@
+# Order People by age Using Arrow Function
+# Sort and Order people by their age using Arrow Functions
+# Task
+# Your task is to order a list containg people objects by age using the new Javascript Arrow Functions
+# Input
+# Input will be a valid array with People objects containing an Age and Name
+# Output
+# Output will be a valid sorted array with People objects sorted by Age in ascending order
+# Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
