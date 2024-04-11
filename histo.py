c79c9708eba7bc204c4200cea32bccbc6b1024fa
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-11 23:11:55 +0100

    teste

--- histo.py
+++ histo.py
@@ -14,3 +14,9 @@ def histogram(lst, w):
     lst = [n // w for n in lst]
     m = max(lst, default=-1) + 1
     return [lst.count(n) for n in range(m)]
+
+
+test.assert_equals(histogram([1, 1, 0, 1, 3, 2, 6], 1), [1, 3, 1, 1, 0, 0, 1])
+test.assert_equals(histogram([1, 1, 0, 1, 3, 2, 6], 2), [4, 2, 0, 1])
+test.assert_equals(histogram([], 1), [])
+test.assert_equals(histogram([8], 1), [0, 0, 0, 0, 0, 0, 0, 0, 1])
