4c865619f3d0b8e8545378d0c08998c0c1537de8
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-13 23:18:31 +0100

    structure

---
+++ howLong.py
@@ -0,0 +1,25 @@
+# Input
+# You'll be given 4 arguments:
+
+# 1. needed power
+# The power of the needed microwave.
+# Example: "600W"
+
+# 2. minutes
+# The number of minutes shown on the package.
+# Example: 4
+
+# 3. seconds
+# The number of seconds shown on the package.
+# Example: 20
+
+# 4. power
+# The power of your microwave.
+# Example: "800W"
+
+# Output
+# The amount of time you should cook the meal for formatted as a string.
+# Example: "3 minutes 15 seconds"
+
+# Note: the result should be rounded up.
+
