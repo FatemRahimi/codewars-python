9aa26af50af4ef8c6a73b39ad131c6442ba6f829
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-18 23:49:26 +0100

    solution

--- RockPaper.py
+++ RockPaper.py
@@ -3,6 +3,14 @@
 
 # Examples(Input1, Input2 --> Output):
 
+def rps(p1, p2):
+    beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    if beats[p1] == p2:
+        return "Player 1 won!"
+    if beats[p2] == p1:
+        return "Player 2 won!"
+    return "Draw!"
+
 # "scissors", "paper" --> "Player 1 won!"
 # "scissors", "rock" --> "Player 2 won!"
 # "paper", "paper" --> "Draw!"
