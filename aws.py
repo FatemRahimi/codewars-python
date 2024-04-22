da6e8121aec073ad9d5c05b76adc7014c94e32c6
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-22 22:42:11 +0100

    create fle

--- aws.py
+++ aws.py
@@ -1,14 +1,14 @@
 
-import json
+# import json
 
-def readJsonFile(fileName):
-    data = ""
-    try:
-        with open(fileName) as json_file:
-            data = json.load(json_file)
-    except IOError:
-        print("Could not read file")
-    return data
+# def readJsonFile(fileName):
+#     data = ""
+#     try:
+#         with open(fileName) as json_file:
+#             data = json.load(json_file)
+#     except IOError:
+#         print("Could not read file")
+#     return data
     
 import jsonFileHandler
 
