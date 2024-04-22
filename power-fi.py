9b8573061f77dde23af1bff3ba294541395256f3
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-22 22:43:18 +0100

    struction

---
+++ power-fi.py
@@ -0,0 +1,23 @@
+DESCRIPTION:
+𝑖
+i is the imaginary unit, it is defined by 
+𝑖
+²
+=
+−
+1
+i²=−1, therefore it is a solution to 
+𝑥
+²
++
+1
+=
+0
+x²+1=0.
+
+Your Task
+Complete the function pofi that returns 
+𝑖
+i to the power of a given non-negative integer in its simplest form, as a string (answer may contain 
+𝑖
+i).
