94e8f0266fff810be6ed91a971cdac19d78f7d79
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-22 22:44:47 +0100

    solution

--- power-fi.py
+++ power-fi.py
@@ -1,23 +1,41 @@
-DESCRIPTION:
-𝑖
-i is the imaginary unit, it is defined by 
-𝑖
-²
-=
-−
-1
-i²=−1, therefore it is a solution to 
-𝑥
-²
-+
-1
-=
-0
-x²+1=0.
+# DESCRIPTION:
+# 𝑖
+# i is the imaginary unit, it is defined by 
+# 𝑖
+# ²
+# =
+# −
+# 1
+# i²=−1, therefore it is a solution to 
+# 𝑥
+# ²
+# +
+# 1
+# =
+# 0
+# x²+1=0.
 
-Your Task
-Complete the function pofi that returns 
-𝑖
-i to the power of a given non-negative integer in its simplest form, as a string (answer may contain 
-𝑖
-i).
+# Your Task
+# Complete the function pofi that returns 
+# 𝑖
+# i to the power of a given non-negative integer in its simplest form, as a string (answer may contain 
+# 𝑖
+# i).
+
+def pofi(n):
+    if n % 4 == 0:
+        return '1'
+    
+    if n % 4 == 1:
+        return 'i'
+    
+    if n % 4 == 2:
+        return '-1'
+    
+    return '-i'
+
+
+#     even -1
+#     odd  i
+#     0    1
+#     return 'i'
