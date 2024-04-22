5e904b1916b8aa228a5832545feb638fdf6f9871
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-22 22:48:14 +0100

    test

--- power-fi.py
+++ power-fi.py
@@ -43,3 +43,24 @@ def pofi(n):
 # another way
 # def pofi(n):
 #     return ['1','i','-1','-i'][n%4]
+
+
+import codewars_test as test
+from solution import pofi
+
+@test.describe("Basic Tests")
+def basic_tests():
+    
+    @test.it("Basic Tests")
+    def basic_tests():
+        test.assert_equals(pofi(0),'1')
+        test.assert_equals(pofi(1),'i')
+        test.assert_equals(pofi(2),'-1')
+        test.assert_equals(pofi(3),'-i')
+        test.assert_equals(pofi(4),'1')
+        test.assert_equals(pofi(5),'i')
+        test.assert_equals(pofi(6),'-1')
+        test.assert_equals(pofi(7),'-i')
+        test.assert_equals(pofi(8),'1')
+        test.assert_equals(pofi(9),'i')
+        test.assert_equals(pofi(10),'-1')
