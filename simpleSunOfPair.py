1826389d8bf3cad122ee2800f33d3b854532b50c
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-24 22:26:37 +0100

    solution

--- simpleSunOfPair.py
+++ simpleSunOfPair.py
@@ -12,3 +12,10 @@
 # More examples in test cases.
 
 # Good luck!
+
+def solve(n):
+    if n < 10:
+        return n
+    a = int((len(str(n)) - 1) * '9')
+    b = n - a
+    return sum([int(i) for i in (list(str(a)) + list(str(b)))])
