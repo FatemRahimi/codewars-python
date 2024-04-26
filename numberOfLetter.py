5eebccbb3c8cadc3ecfd21b0ec7eba5f2002588d
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-26 23:52:11 +0100

    solution

--- numberOfLetter.py
+++ numberOfLetter.py
@@ -7,3 +7,13 @@
 # Examples
 # numbersOfLetters(60) --> ["sixzero", "seven", "five", "four"]
 # numbersOfLetters(1) --> ["one", "three", "five", "four"]
+
+words = "zero one two three four five six seven eight nine".split()
+wordify = lambda n: "".join(words[int(d)] for d in str(n))
+
+
+def numbers_of_letters(n):
+    result = [wordify(n)]
+    while result[-1] != "four":
+        result.append(wordify(len(result[-1])))
+    return result
