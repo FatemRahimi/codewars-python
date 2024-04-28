832350ac5e904adce1e5214d70ebe79060943a3e
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-04-28 22:17:50 +0100

    solution

--- theGeanBottle.py
+++ theGeanBottle.py
@@ -27,3 +27,16 @@
 # parameter n is 1 to 10
 # newline terminates every line (including the last)
 # don't forget the blank lines between the verses
+
+def ten_green_bottles(n):
+    numbers = {10: 'ten', 9: 'nine', 8: 'eight', 7: 'seven', 6: 'six', 5: 'five',
+               4: 'four', 3: 'three', 2: 'two', 1: 'one', 0: 'no'}
+    res = []
+    for x in range(n, 0, -1):
+        s = f"""{numbers[x].capitalize()} green bottle{'s' if x > 1 else ''} hanging on the wall,
+{numbers[x].capitalize()} green bottle{'s' if x > 1 else ''} hanging on the wall,
+{'And if' if x > 1 else "If that"} one green bottle should accidentally fall,
+There'll be {numbers[x-1]} green bottle{'' if x-1 == 1 else 's'} hanging on the wall.
+"""
+        res.append(s)
+    return '\n'.join(res)
