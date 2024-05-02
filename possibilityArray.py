59cdf06b85bbf253e4b46404baf4b01a63c7fc67
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-02 20:23:55 +0100

    test

--- possibilityArray.py
+++ possibilityArray.py
@@ -8,3 +8,21 @@ def is_all_possibilities(arr):
         if arr[i] != i:
             return False
     return True
+
+
+import codewars_test as test
+from solution import is_all_possibilities
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(is_all_possibilities([0,2,19,4,4]),False)
+        test.assert_equals(is_all_possibilities([3,2,1,0]),True)
+        test.assert_equals(is_all_possibilities([0,1,2,3]),True)
+        test.assert_equals(is_all_possibilities([1,2,3,4]),False)
+        test.assert_equals(is_all_possibilities([0,2,3]),False)
+        test.assert_equals(is_all_possibilities([0]),True)
+        test.assert_equals(is_all_possibilities([0,1,2,3,4,5,6,7,8,9]),True)
+        test.assert_equals(is_all_possibilities([0,1,3,-2,5,4]),False)
+        test.assert_equals(is_all_possibilities([1,-1,2,-2,3,-3,6]),False)
