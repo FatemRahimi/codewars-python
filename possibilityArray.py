69d13d49ba857ac622f890704ff269fa2c5b7954
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-02 20:21:17 +0100

    create structure

---
+++ possibilityArray.py
@@ -0,0 +1,14 @@
+# A non-empty array a of length n is called an array of all possibilities if it contains all numbers between 0 and a.length - 1 (both inclusive).
+
+# Write a function that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+# Examples:
+
+# [1,2,0,3] => True
+# # Includes all numbers between 0 and a.length - 1 (4 - 1 = 3)
+
+# [0,1,2,2,3] => False
+# # Doesn't include all numbers between 0 and a.length - 1 (5 - 1 = 4)
+
+# [0] => True
+# # Includes all numbers between 0 and a.length - 1 (1 - 1 = 0).
