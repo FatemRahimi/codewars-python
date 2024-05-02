e653501d437452d4753f497428284cdcce6e87b9
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-02 20:29:36 +0100

    test

--- thePonyExpress.py
+++ thePonyExpress.py
@@ -27,3 +27,10 @@ def riders(stations):
             travelled += dist
     
     return riders
+
+@test.describe("Sample tests")
+def testEx():
+    test.assert_equals(riders([18, 15]), 1)
+    test.assert_equals(riders([43, 23, 40, 13]), 2)
+    test.assert_equals(riders([33, 8, 16, 47, 30, 30, 46]), 3)
+    test.assert_equals(riders([6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49]), 4)
