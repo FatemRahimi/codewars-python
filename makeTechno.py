7ea16c909b9b6ee4c2f9170a3fc88e5d118ae0c2
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-03 23:40:46 +0100

    test

--- makeTechno.py
+++ makeTechno.py
@@ -31,3 +31,16 @@ def compress(sentence):
         if i not in ref:
             ref.append(i)
     return ''.join([str(ref.index(n)) for n in sentence.lower().split()])
+
+
+import codewars_test as test
+from solution import compress
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(compress("The bumble bee"), "012")
+        test.assert_equals(compress("SILLY LITTLE BOYS silly little boys"), "012012")
+        test.assert_equals(compress("Ask not what your COUNTRY can do for you ASK WHAT YOU CAN DO FOR YOUR country"), "01234567802856734")
+        test.assert_equals(compress("The number 0 is such a strange number Strangely it has zero meaning"), "012345617891011")
