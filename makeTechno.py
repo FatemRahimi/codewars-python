27c7724123d63579aea4c0a56fa2deeb8d1c34eb
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-03 23:40:12 +0100

    solution

--- makeTechno.py
+++ makeTechno.py
@@ -25,3 +25,9 @@
 
 # "01234567802856734"
 
+def compress(sentence):
+    ref = []
+    for i in sentence.lower().split():
+        if i not in ref:
+            ref.append(i)
+    return ''.join([str(ref.index(n)) for n in sentence.lower().split()])
