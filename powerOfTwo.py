09bb020ffd7765b70842e0e909240da7285fdb3a
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-05 23:04:00 +0100

    test

--- powerOfTwo.py
+++ powerOfTwo.py
@@ -11,3 +11,14 @@ def powers_of_two(n):
     for i in range(0, n + 1):
         a.append(2 ** i)    
     return a
+
+import codewars_test as test
+from solution import powers_of_two
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(powers_of_two(0), [1])
+        test.assert_equals(powers_of_two(1), [1, 2])
+        test.assert_equals(powers_of_two(4), [1, 2, 4, 8, 16])
