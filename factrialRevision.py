856a6b074e19b0897b5b528abc15ccb2de0c9ba4
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-05 22:58:13 +0100

    create file

--- factrialRevision.py
+++ factrialRevision.py
@@ -1,19 +1,12 @@
-# An array will be passed to the function falsyOrTruthy() containing either both falsy and truthy values or only one type of them; if the number of the array's values is even return truthy values otherwise return falsy ones. In the cases when corresponding values are not present at all simply return an empty array.
+# You have to define a function to calculate the division of two factorial numbers. This can be useful for large numbers.
 
-# Examples:
+# In mathematics, the factorial of integer n is written as n!. It is equal to the product of n and every integer preceding it. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120.
 
-# var arr = [false,NaN,NaN,4,5,{}];
+# Some hints: We can see that (n + 1)! = (n + 1) * n!. So (n + 1)! / n! = (n + 1)
 
-# falsyOrTruthy(arr); // should return [4,5,{}]
+# We know that 0! = 1 (because n! = (n + 1)! / (n + 1) therefore 0! = 1! / 1 = 1)
 
-# var arr = [false,NaN,NaN,4,5,1,{}];
+# And your solution should be able to calculate n! / d!.
 
-# falsyOrTruthy(arr); // should return [false,NaN,NaN]
+# *** Please, don't worry about the parameters. You will only receive positive integers, and the first one will be greater than the second one
 
-# var arr = [false,NaN];
-
-# falsyOrTruthy(arr) // should return [];
-
-# var arr = [1,2,3];
-
-# falsyOrTruthy(arr) // should return [];
