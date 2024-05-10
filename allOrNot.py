fd2a7478a45bd4d08f91d8192380647c23599749
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-10 23:27:30 +0100

    test

--- allOrNot.py
+++ allOrNot.py
@@ -63,3 +63,28 @@ def possibly_perfect(solutions, answers):
             correct += 1
             
     return correct == 0 or correct == (len(solutions) - pass_ans)
+
+import codewars_test as test
+from solution import possibly_perfect
+@test.describe('Fixed Tests')
+def fixed_tests():
+    @test.it('Simple Cases')
+    def example_cases():
+        test.assert_equals(possibly_perfect(['A', '_', 'C', '_', 'B'], ['A', 'D', 'C', 'E', 'B']), True)
+        test.assert_equals(possibly_perfect(['B', '_', 'B'], ['B', 'D', 'C']), False)
+        test.assert_equals(possibly_perfect(['T', '_', 'F', 'F', 'F'], ['F', 'F', 'T', 'T', 'T']), True)
+
+        test.assert_equals(possibly_perfect(['B', 'A', '_', '_'], ['B', 'A', 'C', 'C']), True)
+        test.assert_equals(possibly_perfect(['A', 'B', 'A', '_'], ['B', 'A', 'C', 'C']), True)
+        test.assert_equals(possibly_perfect(['A', 'B', 'C', '_'], ['B', 'A', 'C', 'C']), False)
+
+        test.assert_equals(possibly_perfect(['B', '_'], ['C', 'A']), True)
+        test.assert_equals(possibly_perfect(['B', 'A'], ['C', 'A']), False)
+        test.assert_equals(possibly_perfect(['B'], ['B']), True)
+
+        test.assert_equals(possibly_perfect(['_', 'T', '_', '_'], ['T', 'F', 'F', 'F']), True)
+        test.assert_equals(possibly_perfect(['_', 'T', '_', '_'], ['T', 'T', 'F', 'T']), True)
+        test.assert_equals(possibly_perfect(['_', 'T', 'T', 'T'], ['T', 'T', 'F', 'T']), False)
+        test.assert_equals(possibly_perfect(['_', 'T', 'T', 'T'], ['T', 'T', 'T', 'T']), True)
+        test.assert_equals(possibly_perfect(['_', 'T', 'T', 'T'], ['F', 'F', 'F', 'F']), True)
+        test.assert_equals(possibly_perfect(['_', '_', '_', '_'], ['F', 'F', 'F', 'F']), True)
