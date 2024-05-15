18dc029faf068d440f39c6eeb5b232b8a3d3e4b4
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-15 22:14:28 +0100

    tested

--- drowmw.py
+++ drowmw.py
@@ -38,3 +38,11 @@ def chess_board(rows, columns):
                 l.append('X')
         ans.append(l)
     return ans
+
+@test.it("Basic Tests")
+    def basic_tests():
+        test.assert_equals(chess_board(1, 1),[["O"]])
+        test.assert_equals(chess_board(1, 2),[["O","X"]])
+        test.assert_equals(chess_board(2, 1),[["O"],["X"]])
+        test.assert_equals(chess_board(2, 2),[["O","X"],["X","O"]])
+        test.assert_equals(chess_board(6, 6),[['O','X','O','X','O','X'],['X','O','X','O','X','O'],['O','X','O','X','O','X'],['X','O','X','O','X','O'],['O','X','O','X','O','X'],['X','O','X','O','X','O']])
