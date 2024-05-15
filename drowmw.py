3527660b5e0140bd892ae6e2569be5881ae82a5a
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-15 22:13:30 +0100

    solution

--- drowmw.py
+++ drowmw.py
@@ -26,4 +26,15 @@
 
 # and the black an: 'X'
 
-# The first row should always start with a white space 'O'
+
+def chess_board(rows, columns):
+    ans=[]
+    for i in range(1,rows+1,1):
+        l=[]
+        for j in range(i,columns+i,1):
+            if j%2!=0:
+                l.append('O')
+            else:
+                l.append('X')
+        ans.append(l)
+    return ans
