47bc9ea6b5e211dc31ddb79eec422328e2a1c095
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-16 23:21:27 +0100

    tested

--- drowmw.py
+++ drowmw.py
@@ -34,6 +34,22 @@ def divisors(num):
     return l
 
 
+import codewars_test as test
+from solution import divisors
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(divisors(15), [3,5])
+        test.assert_equals(divisors(253), [11,23])
+        test.assert_equals(divisors(24), [2,3,4,6,8,12])
+        test.assert_equals(divisors(25), [5])
+        test.assert_equals(divisors(13), "13 is prime")
+        test.assert_equals(divisors(3), "3 is prime")
+        test.assert_equals(divisors(29), "29 is prime")
+
+
 def chess_board(rows, columns):
     ans=[]
     for i in range(1,rows+1,1):
