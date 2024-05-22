434b2bddeaec64ab6f46c0a601294c052944ebe1
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-22 23:26:23 +0100

    tested

--- findOdd.py
+++ findOdd.py
@@ -13,3 +13,49 @@ def find_it(seq):
     for elem in set(seq):
         if seq.count(elem) % 2 == 1:
             return elem
+        
+
+
+        def find_it(seq):
+    nums = set()
+    for num in seq:
+        if num in nums:
+            nums.remove(num)
+        else:
+            nums.add(num)
+    return nums.pop()
+        
+
+        import codewars_test as test
+from solution import find_it
+
+@test.describe("Sample tests")
+def sample_tests():
+    
+    @test.it("find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) should return 5 (because it appears 3 times)")
+    def _():
+        test.assert_equals(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
+        
+    @test.it("find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) should return -1 (because it appears 1 time)")
+    def _():
+        test.assert_equals(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1);
+        
+    @test.it("find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) should return 5 (because it appears 3 times)")
+    def _():
+        test.assert_equals(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5);
+        
+    @test.it("find_it([10]) should return 10 (because it appears 1 time)")
+    def _():
+        test.assert_equals(find_it([10]), 10);
+
+    @test.it("find_it([10, 10, 10]) should return 10 (because it appears 3 times)")
+    def _():
+        test.assert_equals(find_it([10, 10, 10]), 10);        
+        
+    @test.it("find_it([1,1,1,1,1,1,10,1,1,1,1]) should return 10 (because it appears 1 time)")
+    def _():
+        test.assert_equals(find_it([1,1,1,1,1,1,10,1,1,1,1]), 10);
+
+    @test.it("find_it([5,4,3,2,1,5,4,3,2,10,10]) should return 1 (because it appears 1 time)")
+    def _():
+       
