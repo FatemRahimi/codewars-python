f237bdacca1951ee1129723f330fc1069fa04745
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-24 23:50:02 +0100

    test

--- gaDe.py
+++ gaDe.py
@@ -17,3 +17,13 @@ def encode(message):
     
 def decode(message):
     return encode(message)
+
+
+
+  def basic_test_cases():
+        test.assert_equals(encode("ABCD") , "GBCE", "Expected 'GBCE'")
+        test.assert_equals(encode("GBCE") , "ABCD", "Expected 'ABCD'")
+        test.assert_equals(encode("Gug hgs g cgt") , "Ala has a cat", "Expected 'Ala has a cat'")
+        test.assert_equals(decode("agedyropulik") , "gaderypoluki", "Expected 'gaderypoluki'")
+        test.assert_equals(decode("Ala has a cat") , "Gug hgs g cgt", "Expected 'Gug hgs g cgt'")
+        test.assert_equals(decode("gaderypoluki") , "agedyropulik", "Expected 'agedyropulik'")
