3590d68280f6319402958d1fcc67fdade935bcf7
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-27 23:57:46 +0100

    solution

--- getAdd.py
+++ getAdd.py
@@ -35,4 +35,47 @@ from itertools import combinations
 
 def getting_mad(arr):
     return  min(abs(a-b) for a,b in combinations(arr,2))
-  
+  import codewars_test as test
+from solution import getting_mad
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it("should return minimum absolute difference for simple cases")
+    def basic_test_cases():
+        arr = [-10,0,-3,1]
+        res = 1
+        test.assert_equals(getting_mad(arr), res)
+
+    @test.it("should works for zero filled arr")
+    def basic_test_cases():
+
+        arr = [0,0,0,0]
+        res = 0
+        test.assert_equals(getting_mad(arr), res)
+
+    @test.it("should works for positive and negative numbers")
+    def basic_test_cases():
+
+        arr = [-570, 542]
+        res = 1112
+        test.assert_equals(getting_mad(arr), res)
+
+        arr = [-69, -808, 828, 57]
+        res = 126
+        test.assert_equals(getting_mad(arr), res)
+
+        arr = [-425, -648, -371, 755, -140, -855, 277, 517, 667, 192, -409, -326]
+        res = 16
+        test.assert_equals(getting_mad(arr), res)
+
+    @test.it("should return minimum absolute difference for positive numbers")
+    def basic_test_cases():
+        arr = [1,2,5,6,7]
+        res = 1
+        test.assert_equals(getting_mad(arr), res)
+
+    @test.it("should return minimum absolute difference for negative numbers")
+    def basic_test_cases():
+        arr = [-1,-2,-5,-6,-7]
+        res = 1
+        test.assert_equals(getting_mad(arr), res)
