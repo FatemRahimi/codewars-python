1bd8b1d8fd447b96a0c8f71aef254a0a57a972a5
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-05-31 00:31:26 +0100

    tets

--- diffrence.py
+++ diffrence.py
@@ -21,3 +21,12 @@ def twos_difference(arr):
             if arr[j] - arr[i] == 2:
                 b.append((arr[i],arr[j]))
     return b
+test.assert_equals(twos_difference([1, 2, 3, 4]), [(1, 3), (2, 4)])
+test.assert_equals(twos_difference([1, 3, 4, 6]), [(1, 3), (4, 6)])
+test.assert_equals(twos_difference([0, 3, 1, 4]), [(1, 3)])
+test.assert_equals(twos_difference([4, 1, 2, 3]), [(1, 3), (2, 4)])
+test.assert_equals(twos_difference([6, 3, 4, 1, 5]), [(1, 3), (3, 5), (4, 6)])
+test.assert_equals(twos_difference([3, 1, 6, 4]), [(1, 3), (4, 6)])
+test.assert_equals(twos_difference([1, 3, 5, 6, 8, 10, 15, 32, 12, 14, 56]), [(1, 3), (3, 5), (6, 8), (8, 10), (10, 12), (12, 14)])
+test.assert_equals(twos_difference([1, 4, 7, 10]), [])
+test.assert_equals(twos_difference([]), [])
