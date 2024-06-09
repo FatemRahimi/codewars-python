30c880d48e36d301fae13110d87f6633fb1a3ced
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-06-09 23:35:42 +0100

    function

--- alocat.py
+++ alocat.py
@@ -24,3 +24,54 @@ def allocate_rooms(customers):
         dep[n] = d
         res[i] = n + 1
     return res
+
+
+
+
+
+import codewars_test as test
+from solution import allocate_rooms
+from preloaded import validate_solution
+
+@test.describe('Example tests')
+def example_tests():
+    
+    test_cases = [ 
+                 [(1,2),(2,4),(4,4)],        ## [1,2,1] or [2,1,2]
+                 [(1,5),(2,4),(6,8),(7,7)],  ## any of [1,2,1,2], [1,2,2,1], [2,1,2,1], or [2,1,1,2]
+                 [(15,22),(2,4),(6,9),(3,33),(12,21)], ## [1,2,2,3,2], [2,1,1,3,1], [3,1,3,2,1], etc
+        
+                 [(1,10),(2,5),(6,6),(3,7),(6,6),(11,13),(9,15),(8,14)], 
+                     ## Solutions include:  [1,2,2,3,4,1,3,2], [1,2,2,3,4,1,2,3], 
+                     ##                     [1,2,4,3,2,1,3,2], [2,3,3,1,4,2,1,3], and others
+                 
+                 [(8,8),(5,8),(8,9),(1,4),(1,3),(5,7),(4,8),(2,2),(4,5),(6,8)]
+                     ## Solutions include:  [4, 1, 5, 1, 2, 4, 2, 3, 3, 3]
+                     ##                     [5, 4, 2, 2, 1, 2, 3, 3, 1, 1], and others
+                 ]
+    
+    rooms_needed = [2,2,3,4,5]
+    
+    for i in range(len(test_cases)):
+        this_test = test_cases[i]
+        result, message = validate_solution(this_test, allocate_rooms(this_test), rooms_needed[i])
+        test.expect(result, message)
+        
+
+@test.describe('Special Cases')
+def special_cases():
+    
+    ## One customer - solution is [1]
+    special_case1 = [(5,100)]
+    result, message = validate_solution(special_case1, allocate_rooms(special_case1), 1)
+    test.expect(result, message)
+        
+    ## Non-overlapping customers, only 1 room needed. Solution is [1,1,1,1,1,1,1]
+    special_case2 = [(15,19),(1,2),(3,5),(10,10),(6,9),(20,99),(101,101)]
+    result, message = validate_solution(special_case2, allocate_rooms(special_case2), 1)
+    test.expect(result, message)
+    
+    ## All customers overlap, so each needs a new room - solution is any permutation of [1,2,3,4,5]
+    special_case3 = [(4,7),(1,10),(2,5),(3,4),(3,12)]
+    result, message = validate_solution(special_case3, allocate_rooms(special_case3), 5)
+    test.expect(result, message) 
