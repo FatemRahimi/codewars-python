aea974dd767746f945fc8d0f9544e7325c807441
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-06-16 23:36:33 +0100

    solution

--- toCube.py
+++ toCube.py
@@ -8,3 +8,21 @@
 
 # has_two_cube_sums(1729); // true
 # has_two_cube_sums(42);   // false
+
+
+
+def has_two_cube_sums(n):
+    sum = []
+    power = int(n ** (1/3.0))+1
+    for i in xrange(1, power):
+        for x in xrange(1, power):
+            if i == x:
+                continue
+            tmp = i**3 + x**3
+            if tmp > n: 
+               break
+            if tmp == n:
+                if len(sum) == 2:
+                    return True
+                sum.append([i , x])
+    return False
