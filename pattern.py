c050f70a97eef2ae6b3afac831ed1c85a9f42609
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-06-23 23:18:25 +0100

    solution

--- pattern.py
+++ pattern.py
@@ -46,3 +46,16 @@
 # You can see, always a root, always steps of hight 3, tree never smaller than 3 (return "") and no difference for input values like 15 or 17 (because (int) 15/3 = (int) 17/3). That's valid for every input and every tree. Lines are delimited by \r\n and no trailing white space allowed. I think there's nothing more to say - perhaps look at the testcases too;-)!
 
 # There are some static tests at the beginning and many random tests if you submit your solution.
+
+def christmas_tree(height: int) -> str:
+    if height < 3:
+        return ''
+
+    lines = []
+    for i in range(1, height // 3 * 2 + 1, 2):
+        for j in range(i, i + 6, 2):
+            lines.append('*' * j)
+
+    line_width = len(lines[-1])
+    lines.append('###')
+    return '\r\n'.join(line.center(line_width).rstrip() for line in lines)
