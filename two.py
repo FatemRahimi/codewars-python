9eb516004c2d0dbd2e5a7d3699db825578eec6fe
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-06-23 23:14:58 +0100

    create

--- two.py
+++ two.py
@@ -17,3 +17,24 @@ def two_sum(nums, t):
         for j, y in enumerate(nums):
             if i != j and x + y == t:
                 return [i, j]
+            
+
+
+
+sample_test_cases = [
+    #   numbers       target   valid results
+    ([1 ,2, 3],            4,  ((0,2), (2,0))),
+    ([1234,5678,9012], 14690,  ((1,2), (2,1))),
+    ([2, 2, 3],            4,  ((0,1), (1,0))),
+]
+
+@test.describe('Sample tests')
+def sample_tests():
+    for numbers, target, valid_results in sample_test_cases:
+        @test.it(f'two_sums({numbers}, {target})')
+        def _():
+            actual = two_sum(numbers, target)
+            msg = []
+            if isinstance(actual, list): msg.append('The result should be a tuple, not a list')
+            msg.append(f'{actual} should be either {valid_results[0]} or {valid_results[1]}')
+            test.expect(actual in valid_results, '\n'.join(msg))
