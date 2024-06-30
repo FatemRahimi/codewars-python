bda75a85b59c7fb226be92d23090afa2491cfc47
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-06-30 22:41:35 +0100

    creat file

--- bankersplan.py
+++ bankersplan.py
@@ -41,4 +41,14 @@ def fortune(money, interest, withdraw, years, inflation):
         withdraw *= inflation
     return True
 
-    
+    @test.describe('Tests')
+def fixed_tests():
+        
+    @test.it('Basic Tests')
+    def tests():
+        test.assert_equals(fortune(100000, 1, 2000, 15, 1), True)
+        test.assert_equals(fortune(100000, 1, 9185, 12, 1), False)
+        test.assert_equals(fortune(100000000, 1, 100000, 50, 1), True)
+        test.assert_equals(fortune(100000000, 1.5, 10000000, 50, 1), False)
+        test.assert_equals(fortune(100000000, 5, 1000000, 50, 1), True)
+        
