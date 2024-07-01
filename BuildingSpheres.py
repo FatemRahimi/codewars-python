58f423d90b7bf40d9eba0c9f3da4136501b4f1e9
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-07-01 23:20:16 +0100

    testd

--- BuildingSpheres.py
+++ BuildingSpheres.py
@@ -38,3 +38,14 @@ class Sphere(object):
     import codewars_test as test
 from solution import *
 
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        ball = Sphere(2,50)
+
+        test.assert_approx_equals(ball.get_radius(),2, margin=1e-5, message="Check radius")
+        test.assert_approx_equals(ball.get_mass(),50, margin=1e-5, message="Check mass")
+        test.assert_approx_equals(ball.get_volume(), 33.51032, margin=1e-5, message="Check volume")
+        test.assert_approx_equals(ball.get_surface_area(),50.26548, margin=1e-5, message="Check area")
+        test.assert_approx_equals(ball.get_density(),1.49208, margin=1e-5, message="Check density")
